8b8d2c720c7e1d5a3ea657bfbf6a3724bcd8bb31
ricottam <jaskulskim.gdynia@gmail.com>
2024-01-31 11:31:24 +0100

    Designing home page, problem with filling remaining height

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HomeContentComponent } from './components/home-content/home-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, HomeContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition, faHouse } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
-export class NavComponent {}
+export class NavComponent {
+  houseIcon: IconDefinition = faHouse;
+}
