4bab8b912492ac8bcff156efe1e1ca9cb50075ca
ricottam <jaskulskim.gdynia@gmail.com>
2024-01-31 15:38:07 +0100

    Swallow logo, contact icons, font update

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-intro',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.scss',
 })
 export class IntroComponent {
-  text: string = 'Your Angular Developer.';
+  currentText: string = '';
+  private texts: string[] = [
+    'Angular Developer.',
+    'Frontend Software Developer.',
+    'Fullstack Software Developer.',
+    'Computer Science Student.',
+  ];
+  private textIndex: number = 0;
+  private typingInterval: any;
+
+  constructor() {
+    this.printNewText();
+  }
+
+  printNewText(): void {
+    const fullText: string = this.texts[this.textIndex];
+    let currentIndex = 0;
+
+    if (this.typingInterval) {
+      clearInterval(this.typingInterval);
+    }
+
+    this.typingInterval = setInterval(() => {
+      this.currentText = fullText.substring(0, currentIndex + 1);
+      currentIndex++;
+
+      if (currentIndex === fullText.length) {
+        clearInterval(this.typingInterval);
+        this.moveToNextText();
+      }
+    }, 75);
+  }
+
+  moveToNextText(): void {
+    this.textIndex = (this.textIndex + 1) % this.texts.length;
+    setTimeout(() => this.printNewText(), 3000);
+  }
 }
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { IconDefinition, faHouse } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
-export class NavComponent {
-  houseIcon: IconDefinition = faHouse;
-}
+export class NavComponent {}
